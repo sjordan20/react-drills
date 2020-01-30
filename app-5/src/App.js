587aed958a067433ed112a291b6ee6f9725e2d32
sjordan20 <jordanstephen949@gmail.com>
2020-01-29 21:01:34 -0700

    7/10 finished

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Image from './Image.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+  }
+  render() {
+    return (
+      <div className='App'>
+        <Image url={'https://http.cat/409'} />
+      </div>
+    )
+  }
 }
+
+
 
 export default App;
